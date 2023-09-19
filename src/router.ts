d0d71e9fdaf516017d0be99093a1ec1c8ab9ff3a
Israel Sales <israelsales043@gmail.com>
2023-09-19 20:12:01 -0300

    Ready

--- src/router.ts
+++ src/router.ts
@@ -1,26 +1,7 @@
 import { Router } from 'express';
-import { PrismaClient } from '@prisma/client';
-import { Request, Response } from 'express';
 import { loginUser, registerUser } from './controllers/usersControllers';
 
 export const router = Router();
 
-const prisma = new PrismaClient()
-
-export async function getUsers() {
-    const users = await prisma.users.findMany();
-    return users;
-}
-
-
-
-const users = async (req: Request, res: Response) => {
-    const users = await getUsers();
-
-    return res.status(200).json(users)
-}
-
-
-router.get('/', users);
 router.post('/user/register', registerUser);
 router.post('/user/login', loginUser);
