c955aa59de5789c1027e59c49b70b36cd4345500
Israel Sales <israelsales043@gmail.com>
2023-09-19 19:59:44 -0300

    env port

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,11 @@
 import express from 'express';
 import { router } from './router';
+require('dotenv').config();
 
 const app = express();
 
-
-
-
-
-
-
 app.use(express.json());
-
-
 
 app.use(router)
 
-
-
-
-app.listen(3000);
+app.listen(process.env.PORT);
